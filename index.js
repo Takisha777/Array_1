81cc01841a454149d6ed7d8521205a7114c02dd9
Takisha777 <55092730+Takisha777@users.noreply.github.com>
2024-06-02 21:19:27 +0200

    Correct pop and shift

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
-const peopleWaiting = ['Кристина', 'Олег', 'Кирилл', 'Мария', 'Светлана', 'Артем', 'Глеб'];
-
 const giveParcel = (peopleWaiting) => {
-    alert (`${peopleWaiting[0]} получил(а) посылку. В очереди осталось ${peopleWaiting.length - 1} человек.`);
-    return peopleWaiting.shift();
+    alert (`${peopleWaiting.shift()} получил(а) посылку. В очереди осталось ${peopleWaiting.length} человек.`);
 }
 const leaveQueueWithoutParcel = (peopleWaiting) => {
     peopleWaiting.shift();
-    for (let i = 0; i < peopleWaiting.length;) {
-        alert(`${peopleWaiting[0]} не получил(а) посылку и ушел(ла) из очереди`);
-        peopleWaiting.shift();
+    for (let i = 0;  i < peopleWaiting.length;) {
+        alert(`${peopleWaiting.pop()} не получил(а) посылку и ушел(ла) из очереди`);
     }
 }
-console.log(giveParcel(peopleWaiting));
-console.log(giveParcel(peopleWaiting));
-console.log(leaveQueueWithoutParcel(peopleWaiting));
-console.log(peopleWaiting);
+giveParcel(peopleWaiting);
+giveParcel(peopleWaiting);
+leaveQueueWithoutParcel(peopleWaiting);
